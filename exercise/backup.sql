50eda66f99f1cd5848a2bc95ca3b86714b154a7e
Anshika <anshika2020ag@gmail.com>
2025-11-03 14:44:43 +0530

    changes

--- exercise/backup.sql
+++
@@ -1,6 +0,0 @@
--- Command for backup: pg_dump -U postgres -h localhost -p 5432 mydb > mydb_backup.sql
-
--- Create a db to restore
-CREATE DATABASE IF NOT EXISTS mydb_restored;
-
--- Command to restore: psql -U postgres -h localhost -p 5432 mydb_restored -f mydb_backup.sql
