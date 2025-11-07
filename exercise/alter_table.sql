20b4f974af1fcece36bc5e50ce7a6dd9ed714cd7
Anshika <anshika2020ag@gmail.com>
2025-11-07 12:09:59 +0530

    updated

--- exercise/alter_table.sql
+++ exercise/alter_table.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS testing_table;
 
 CREATE TABLE IF NOT EXISTS testing_table (
-    name VARCHAR(30),
-    contact_name VARCHAR(30),
-    roll_no VARCHAR(10) PRIMARY KEY
+    roll_no VARCHAR(10) PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    contact_name VARCHAR(30)
 );
 
 ALTER TABLE testing_table
@@ -13,7 +13,7 @@ ALTER TABLE testing_table
 RENAME contact_name TO username;
 
 ALTER TABLE testing_table
-ADD COLUMN first_name VARCHAR(30);
+ADD COLUMN first_name VARCHAR(30) NOT NULL CHECK (length(first_name) >= 2);
 
 ALTER TABLE testing_table
 ADD COLUMN last_name VARCHAR(30);
